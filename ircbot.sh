c165e24f02a0bc9bf6066e7f9a887c2218dc6d65
ehg <chris.blower@jumpwire.co.uk>
2012-07-01 15:47:19 +0100

    set filebrain path

--- ircbot.sh
+++ ircbot.sh
@@ -5,5 +5,6 @@ export HUBOT_IRC_SERVER='irc.freenode.net'
 export HUBOT_IRC_ROOMS='#ehgtest'
 export HUBOT_IRC_NICK='pearb0t'
 export HUBOT_IRC_UNFLOOD=urgh
+export FILE_BRAIN_PATH=$(pwd)
 
 sh bin/hubot -a irc -n pearb0t
